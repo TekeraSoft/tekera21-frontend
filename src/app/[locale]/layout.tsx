f6cf82c191ac215917e27580b09ec58d2a808f91
raerta <ramazanertan@gmail.com>
2025-05-05 17:10:29 +0300

    layout lang param

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -24,7 +24,7 @@ export default async function RootLayout({
   }
 
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang={locale} suppressHydrationWarning>
       <body className={` antialiased`}>
         <StoreProvider>
           <NextIntlClientProvider>
