789ed2c61c216b0c4bd68152caacab060bcd7a38
raerta <ramazanertan@gmail.com>
2025-05-08 12:15:12 +0300

    colors

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-secondary text-primary-foreground hover:bg-sixth",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90 text-white",
         outline:
