8e0595a6622ca61f44d346243f2f7330208ece17
raerta <ramazanertan@gmail.com>
2025-05-13 17:53:19 +0300

    animate

--- src/components/superadmin/AdminInnerLayout.tsx
+++ src/components/superadmin/AdminInnerLayout.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 const AdminInnerLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="flex flex-col gap-6 lg:px-6 px-1 py-3 w-full">{children}</div>;
+  const [animated, setAnimated] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimated(true);
+    }, 500);
+
+    return () => {
+      setAnimated(false);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`flex flex-col gap-6 lg:px-6 px-1 py-3 w-full transform transition-opacity duration-500  ${
+        animated ? "opacity-100" : "opacity-0"
+      }`}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default AdminInnerLayout;
