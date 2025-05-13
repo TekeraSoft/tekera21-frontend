cfd2f3827e1abb2fef052a44193c655cc2fe0df9
raerta <ramazanertan@gmail.com>
2025-05-13 12:29:37 +0300

    before merge

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,11 +1,27 @@
 "use server";
 
+import axiosInstance from "@/request/axiosInstance";
 import { cookies } from "next/headers";
 
 export async function getUser(layout: string) {
   console.log("get user", layout);
   const cookieStore = await cookies();
   try {
+    // const userFromApi = await axiosInstance.post("")
+    
+    
+    // await fetch("https://dummyjson.com/auth/login", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify({
+    //     username: "emilys",
+    //     password: "emilyspass",
+    //     expiresInMins: 30, // optional, defaults to 60
+    //   }),
+    //   credentials: "include", // Include cookies (e.g., accessToken) in the request
+    // })
+    //   .then((res) => res.json())
+    //   .then(console.log);
     const user = cookieStore.get("user")?.value;
     if (!user) {
       return null;
@@ -20,7 +36,6 @@ export async function getUser(layout: string) {
     return null;
   }
 }
-
 
 export async function logOut() {
   const cookieStore = await cookies();
